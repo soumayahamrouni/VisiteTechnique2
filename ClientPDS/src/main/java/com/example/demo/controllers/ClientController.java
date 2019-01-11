dc202047333231738b442e60862555ca1d0bd6ff
soumayahamrouni <44235862+soumayahamrouni@users.noreply.github.com>
2019-01-11 18:56:11 +0100

    Update ClientController.java

--- ClientPDS/src/main/java/com/example/demo/controllers/ClientController.java
+++ ClientPDS/src/main/java/com/example/demo/controllers/ClientController.java
@@ -10,10 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.com.I_Reservation;
+import com.example.demo.com.I_Vehicule;
 import com.example.demo.dao.ClientRepository;
 import com.example.demo.entities.Client;
 import com.example.demo.entities.Reservation;
-
+import com.example.demo.entities.Vehicule;
 
 @RestController
 @CrossOrigin(origins = { "http://localhost:4200" })
@@ -21,14 +22,12 @@ public class ClientController {
 	@Autowired
 	private ClientRepository clientrepo;
 
-	
-	  
-		@Autowired
+	@Autowired
 	private I_Reservation lc;
-		
-	
-	
-	
+
+	@Autowired
+	private I_Vehicule vh;
+	@CrossOrigin(origins = { "http://localhost:4200" })
 	@RequestMapping("/save")
 	public Client saveclient(Client c) {
 		clientrepo.save(c);
@@ -42,26 +41,39 @@ public class ClientController {
 	}
 
 	@RequestMapping("/Clientpds")
-	public Optional<Client> getById(String id){		
+	public Optional<Client> getById(String id) {
 
-		Optional<Client> cmd= clientrepo.findById  (id);
-		List<Reservation> lgnes=lc.GetReservation(id);
-		cmd.get().setReslist(lgnes);
-		return cmd;
+		Optional<Client> c = clientrepo.findByIdClient(id);
+		List<Reservation> lgnes = lc.GetReservation(id);
+		List<Vehicule> vhs = vh.GetVehicules(id);
+
+		c.get().setVehiculeslist(vhs);
+		c.get().setReslist(lgnes);
+		return c;
 	}
-	
-	
-	@RequestMapping("/liste")
-	public List<String> getcode(){		
 
-		List<Client> cmd= clientrepo.findAll();
-		
-		List<String> array=new ArrayList<String>();
+	@RequestMapping("/liste")
+	public List<String> getcode() {
+
+		List<Client> cmd = clientrepo.findAll();
+
+		List<String> array = new ArrayList<String>();
 		for (Client info : cmd) {
 			array.add(info.getIdClient());
 		}
 		return array;
 
 	}
-	
+
+	@RequestMapping("/updateClient")
+	public Client update(Client c) {
+		return clientrepo.save(c);
+	}
+
+	@RequestMapping("/find")
+	public Client getByLoginPassword(String userName, String password) {
+
+		return clientrepo.findByUserNameAndPassword(userName, password);
+	}
+
 }
