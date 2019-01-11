c538aaf800fc49a0f3bfff7d617d485faff9df42
soumayahamrouni <44235862+soumayahamrouni@users.noreply.github.com>
2019-01-11 19:14:06 +0100

    Update VehiculeRepository.java

--- VehiculePDS/src/main/java/com/example/demo/dao/VehiculeRepository.java
+++ VehiculePDS/src/main/java/com/example/demo/dao/VehiculeRepository.java
@@ -6,4 +6,6 @@ import com.example.demo.entities.Vehicule;
 
 public interface VehiculeRepository extends MongoRepository<Vehicule, Long>{
 
+	List<Vehicule> findByClient(String id);
+	Vehicule findByIdVehicule(String id);
 }
