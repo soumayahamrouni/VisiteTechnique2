12b5174b0bd15a69e2d1cf04b5b987de4b07dceb
soumayahamrouni <44235862+soumayahamrouni@users.noreply.github.com>
2019-01-11 18:57:16 +0100

    Update ClientRepository.java

--- ClientPDS/src/main/java/com/example/demo/dao/ClientRepository.java
+++ ClientPDS/src/main/java/com/example/demo/dao/ClientRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.example.demo.entities.Client;
 
 public interface ClientRepository  extends MongoRepository<Client,String> {
-
+  Optional<Client> findByIdClient(String id);
+	Client findByUserNameAndPassword(String userName,String password);
 }
